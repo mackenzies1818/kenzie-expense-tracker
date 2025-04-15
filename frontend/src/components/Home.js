11dd5606ed3ce1d2df134d3293cd09a24cef4d68
mackenzies1818 <41121136+mackenzies1818@users.noreply.github.com>
2025-04-15 12:47:20 -0600

    updating responsiveness (#5)
    Co-authored-by: ¨mackenzies1818¨ <¨mackenzielsheridan@gmail.com¨>

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
       <div className="flex bg-gray-100">
         <Sidebar />
       </div>
-      <div className="ml-64 p-6 flex-1">
+      <div className="sm:ml-12 sm:p-4 sm:flex-none md:ml-64 p-6 flex-1">
         <h1><Header /></h1>
         <ExpensesChart />
         <h2 className ="text-xl font-semibold pb-3">Transactions</h2>
